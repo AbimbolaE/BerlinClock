05f13656dfa7a1e48be7cc1a3c595f0b51b5fb22
Abimbola Esuruoso <abimbolaesuruoso@gmail.com>
2017-05-12 19:18:20 +0100

    Added tests for showing error messages

--- src/main/scala/com/hotels/hackday/BerlinClock.scala
+++ src/main/scala/com/hotels/hackday/BerlinClock.scala
@@ -1,16 +1,28 @@
 package com.hotels.hackday
 
 import scala.io.Source
+import scala.util.matching.Regex
 
 /**
   * Created by Abimbola on 12/05/2017.
   */
 object BerlinClock {
 
+  val timeFormat: Regex = "([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]".r
+
   def main(args: Array[String]): Unit = {
 
-    args match {
-      case _ => println(Source.fromResource("instructions.txt").mkString)
+    args.headOption match {
+      case Some(time) =>
+
+        time match {
+          case timeFormat(hour, minutes, seconds) => ???
+          case _ => showErrorMessage()
+        }
+      case None => showInstructions()
     }
   }
+
+  private def showInstructions() = println(Source.fromResource("instructions.txt").mkString)
+  private def showErrorMessage() = println("Your time is not in the correct format, please correct it (HH:mm:ss)")
 }
--- src/test/scala/com/hotels/hackday/BerlinClockSpec.scala
+++ src/test/scala/com/hotels/hackday/BerlinClockSpec.scala
@@ -11,6 +11,8 @@ import scala.io.Source
   */
 class BerlinClockSpec extends WordSpec {
 
+  val lineSeparator = sys.props("line.separator")
+
   "A Berlin Clock application" should {
 
     "print instructions if the time is missing" in {
@@ -20,8 +22,30 @@ class BerlinClockSpec extends WordSpec {
         BerlinClock.main(Array.empty[String])
       }
 
-      val instructions = Source.fromResource("instructions.txt").mkString + sys.props("line.separator")
+      val instructions = Source.fromResource("instructions.txt").mkString + lineSeparator
       assert(stream.toString === instructions)
+    }
+
+    "print an error message if the time is in the wrong format" in {
+
+      val stream = new ByteArrayOutputStream()
+      Console.withOut(stream) {
+        BerlinClock.main(Array("badly formatted time"))
+      }
+
+      val errorMessage = "Your time is not in the correct format, please correct it (HH:mm:ss)" + lineSeparator
+      assert(stream.toString === errorMessage)
+    }
+
+    "print an error message if the time is in the right format but is invalid" in {
+
+      val stream = new ByteArrayOutputStream()
+      Console.withOut(stream) {
+        BerlinClock.main(Array("99:99:99"))
+      }
+
+      val errorMessage = "Your time is not in the correct format, please correct it (HH:mm:ss)" + lineSeparator
+      assert(stream.toString === errorMessage)
     }
   }
 }
